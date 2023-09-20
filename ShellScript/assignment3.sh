836b90ef99d12641d584c385d21de7d0389cc34c
rineeee <harin14@naver.com>
2023-09-20 21:13:47 +0900

    feat: add validate parameters

--- ShellScript/assignment3.sh
+++ ShellScript/assignment3.sh
@@ -1,13 +1,31 @@
 #! /bin/sh
 
-result=0
-for j in $(seq 1 $1)
-do
-	for i in $(seq 1 $2)
-        do
-                result=`expr $j \* $i`
-                echo -n "$j * $i = $result \t"
+validate(){
+	if [ -z $1 ] || [ -z $2 ] ; then
+		echo "Invalid Input"
+		exit 0
+	fi
+
+	if [ $1 -lt 1 ] || [ $2 -lt 1 ] ; then
+		echo "Input must be greater than 0"
+		exit 0
+	fi
+}
+
+calc() {
+	validate $1 $2
+
+	result=0
+	for j in $(seq 1 $1)
+	do
+		for i in $(seq 1 $2)
+        	do
+                	result=`expr $j \* $i`
+                	echo -n "$j * $i = $result \t"
         done
 
-        echo " " 
-done
+       		 echo " " 
+	done
+}
+
+calc $1 $2
